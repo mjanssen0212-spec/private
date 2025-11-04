2852f32c8deb94a686e45c44f82d91e1addfb315
mjanssen0212-spec <m.janssen0212@gmail.com>
2025-11-04 09:13:45 +0100

    .

---
+++ Universität/Übersetzung und Analyse von Programmen/Projekte/Projekt_1/uap25-pro01-tram/src/de/unitrier/st/uap/w25/tram/AbstractMachine.java
@@ -0,0 +1,287 @@
+package de.unitrier.st.uap.w25.tram;
+//Matthias Janßen
+//1871808
+
+import java.util.List;
+
+import static de.unitrier.st.uap.w25.tram.Main.isDebug;
+import static de.unitrier.st.uap.w25.tram.Main.logger;
+
+public class AbstractMachine {
+    private List<Integer> stack;
+    private int TOP;
+    //parameter pointer
+    private int PP;
+    //Frame Pointer
+    private int FP;
+    //Program counter
+    private int PC;
+
+    public AbstractMachine(List<Integer> stack, int TOP, int PP, int FP, int PC) {
+        this.stack = stack;
+        this.TOP = TOP;
+        this.PP = PP;
+        this.FP = FP;
+        this.PC = PC;
+    }
+
+    public List<Integer> runProgram(Instruction[] instructions) {
+        while (PC != -1) {
+
+            if (PC >= instructions.length) {
+                PC = -1;
+                break;
+            }
+            Instruction i = instructions[PC];
+            switch (i.getOpcode()) {
+                case 1:
+                    //CONST
+                    stack.add(TOP + 1, i.getArg1());
+                    TOP++;
+                    PC++;
+                    break;
+                case 2:
+                    //LOAD
+                    stack.add(TOP + 1, stack.get(spp(i.getArg2(), PP, FP) + i.getArg1()));
+                    TOP++;
+                    PC++;
+                    break;
+                case 3:
+                    //STORE
+                    stack.set(spp(i.getArg2(), PP, FP) + i.getArg1(), stack.get(TOP));
+                    stack.remove(TOP);
+                    TOP--;
+                    PC++;
+                    break;
+                case 4:
+                    //ADD
+                    stack.set(TOP - 1, stack.get(TOP - 1) + stack.get(TOP));
+                    stack.remove(TOP);
+                    TOP--;
+                    PC++;
+                    break;
+                case 5:
+                    //SUB
+                    stack.set(TOP - 1, stack.get(TOP - 1) - stack.get(TOP));
+                    stack.remove(TOP);
+                    TOP--;
+                    PC++;
+                    break;
+                case 6:
+                    //MUL
+                    stack.set(TOP - 1, stack.get(TOP - 1) * stack.get(TOP));
+                    stack.remove(TOP);
+                    TOP--;
+                    PC++;
+                    break;
+                case 7:
+                    //DIV
+                    stack.set(TOP - 1, stack.get(TOP - 1) / stack.get(TOP));
+                    stack.remove(TOP - 1);
+                    TOP--;
+                    PC++;
+                    break;
+                case 8:
+                    //LT
+                    if (stack.get(TOP - 1) < stack.get(TOP)) {
+                        stack.set(TOP - 1, 1);
+                    } else {
+                        stack.set(TOP - 1, 0);
+                    }
+                    stack.remove(TOP);
+                    TOP--;
+                    PC++;
+                    break;
+                case 9:
+                    //GT
+                    if (stack.get(TOP - 1) > stack.get(TOP)) {
+                        stack.set(TOP - 1, 1);
+                    } else {
+                        stack.set(TOP - 1, 0);
+                    }
+                    stack.remove(TOP);
+                    TOP--;
+                    PC++;
+                    break;
+                case 10:
+                    //EQ
+                    if (stack.get(TOP - 1) == stack.get(TOP)) {
+                        stack.set(TOP - 1, 1);
+                    } else {
+                        stack.set(TOP - 1, 0);
+                    }
+                    stack.remove(TOP);
+                    TOP--;
+                    PC++;
+                    break;
+                case 11:
+                    //NEQ
+                    if (stack.get(TOP - 1) != stack.get(TOP)) {
+                        stack.set(TOP - 1, 1);
+                    } else {
+                        stack.set(TOP - 1, 0);
+                    }
+                    stack.remove(TOP);
+                    TOP--;
+                    PC++;
+                    break;
+                case 12:
+                    //IFZERO
+                    if (stack.get(TOP) == 0) {
+                        PC = i.getArg1();
+                    } else {
+                        PC++;
+                    }
+                    stack.remove(TOP);
+                    TOP--;
+                    break;
+                case 13:
+                    //GOTO
+                    PC = i.getArg1();
+                    break;
+                case 14:
+                    //HALT
+                    PC = -1;
+                    break;
+                case 15:
+                    //NOP
+                    PC++;
+                    break;
+                case 16:
+                    //INVOKE
+                    stack.add(TOP + 1, PC + 1);
+                    stack.add(TOP + 2, PP);
+                    stack.add(TOP + 3, FP);
+                    stack.add(TOP + 4, spp(i.getArg3(), PP, FP));
+                    stack.add(TOP + 5, sfp(i.getArg3(), PP, FP));
+                    PP = TOP - i.getArg1() + 1;
+                    FP = TOP + 1;
+                    TOP = TOP + 5;
+                    PC = i.getArg2();
+                    break;
+                case 17:
+                    //RETURN
+                    int res = stack.get(TOP);
+                    TOP = PP;
+                    int PCtemp = PC;
+                    PC = stack.get(FP);
+                    PP = stack.get(FP + 1);
+                    FP = stack.get(FP + 2);
+                    stack.set(TOP, res);
+                    while (stack.size() > TOP + 1) {
+                        stack.removeLast();
+                    }
+                    if (stack.size() == 1) {
+                        PC = PCtemp + 1;
+                    }
+                    break;
+                case 18:
+                    //POP
+                    stack.remove(TOP);
+                    TOP--;
+                    PC++;
+                    break;
+            }
+            if (isDebug) {
+                logger.debug(formatHelper(
+                        i, stack
+                ));
+            }
+        }
+        if (isDebug) {
+            logger.debug("PP={}, FP={}, PC={}, STACK={}", PP, FP, PC, stack);
+        }
+        return stack;
+    }
+
+    private int spp(int d, int pp, int fp) {
+        if (d == 0) {
+            return pp;
+        } else {
+            spp(d - 1, stack.get(fp + 3), stack.get(fp + 4));
+        }
+        return -1;
+    }
+
+    private int sfp(int d, int ppp, int fp) {
+        if (d == 0) {
+            return fp;
+        } else {
+            sfp(d - 1, stack.get(fp + 3), stack.get(fp + 4));
+        }
+        return -1;
+    }
+
+    public List getStack() {
+        return stack;
+    }
+
+    public void setStack(List stack) {
+        this.stack = stack;
+    }
+
+    public int getTOP() {
+        return TOP;
+    }
+
+    public void setTOP(int TOP) {
+        this.TOP = TOP;
+    }
+
+    public int getPP() {
+        return PP;
+    }
+
+    public void setPP(int PP) {
+        this.PP = PP;
+    }
+
+    public int getFP() {
+        return FP;
+    }
+
+    public void setFP(int FP) {
+        this.FP = FP;
+    }
+
+    public int getPC() {
+        return PC;
+    }
+
+    public void setPC(int PC) {
+        this.PC = PC;
+    }
+
+    private String formatHelper(Instruction instr, List<Integer> stack) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(String.format("After instruction = %s; configuration = PC = %d; PP = %d; FP = %d; TOP = %d", instr.toString().split(" ")[0], PC, PP, FP, TOP));
+        sb.append(System.lineSeparator());
+        sb.append("Stack:").append(System.lineSeparator());
+
+        if (TOP == -1) {
+            sb.append("[]").append(System.lineSeparator());
+        }
+        for (int i = 0; i < stack.size(); i++) {
+            sb.append(String.format("[%d] = %d", i, stack.get(i)));
+
+            if ((int) PP == i) {
+                String label = "PP";
+                sb.append(String.format(" <-- %s (%s = %d)", label, label, i));
+            }
+            if (FP == i) {
+                String label = "FP";
+                sb.append(String.format(" <-- %s (%s = %d)", label, label, i));
+            }
+            if (TOP == i) {
+                String label = "TOP";
+                sb.append(String.format(" <-- %s (%s = %d)", label, label, i));
+            }
+
+            sb.append(System.lineSeparator());
+        }
+
+        return sb.toString();
+    }
+
+}
---
+++ Universität/Übersetzung und Analyse von Programmen/Projekte/Projekt_1/uap25-pro01-tram/src/de/unitrier/st/uap/w25/tram/Main.java
@@ -0,0 +1,58 @@
+package de.unitrier.st.uap.w25.tram;
+//Matthias Janßen
+//1871808
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+final class Main {
+    private Main() {
+    }
+
+    protected static final Logger logger = LogManager.getLogger(Main.class);
+    protected static Boolean isDebug = false;
+
+    public static void main(String[] argv) {
+        String filename = "";
+        if (argv.length > 0) {
+            if (argv[0].equals("-d")) {
+                isDebug = true;
+                filename = argv[1];
+            } else {
+                filename = argv[0];
+            }
+            if (filename == null) {
+                filename = "tramcode\\test.tram";
+            }
+        }
+        Instruction[] code = Assembler.readTRAMCode(
+//				"tramcode\\square.tram"
+//				 "tramcode\\wrapper.tram"
+//                 "tramcode\\example1.tram"
+//				 "tramcode\\example2.tram"
+//				 "tramcode\\example3.tram"
+//				"tramcode\\test.tram"
+                filename
+//                "tramcode\\euklid.tram"
+        );
+
+        int lineNr = 0;
+        for (Instruction instr : code) {
+            if (instr != null) {
+                if (isDebug) {
+                    logger.debug(String.format("%03d", lineNr) + "| " + instr.toString());
+                }
+                lineNr++;
+            }
+        }
+
+        // TODO: Create an instance of the abstract machine with reasonable parameters
+        AbstractMachine am = new AbstractMachine(new ArrayList<>(), -1, 0, 0, 0);
+        List<Integer> stack = am.runProgram(code);
+    }
+}
