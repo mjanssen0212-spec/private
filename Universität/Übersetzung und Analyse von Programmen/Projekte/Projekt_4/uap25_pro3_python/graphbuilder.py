356495b60273ebcc028c3abaa83d7d7073e5d89c
mjans <m.janssen0212@gmail.com>
2026-01-24 18:05:29 +0100

    .

--- Universität/Übersetzung und Analyse von Programmen/Projekte/Projekt_4/uap25_pro3_python/graphbuilder.py
+++ Universität/Übersetzung und Analyse von Programmen/Projekte/Projekt_4/uap25_pro3_python/graphbuilder.py
@@ -23,7 +23,7 @@ class CONST(compiler.CONST):
         """
         w = self
         vertices = {w}
-        edges = {}
+        edges = set()
         in_node = w
         out_node = w
         
@@ -50,12 +50,12 @@ class WHILE(compiler.WHILE):
         vertices_1, edges_1, in_node_1, out_node_1 = e1.cfg(psi)
         vertices_2, edges_2, in_node_2, out_node_2 = e2.cfg(psi)
 
-        diamond = 'diamond'
-        glue = 'glue'
+        diamond = ('diamond', w)
+        glue = ('glue', w)
 
         vertices = vertices_1 | vertices_2 | {diamond, glue}
         edges = (edges_1 | edges_2 |
-                 {(out_node_1, '', glue), (diamond, 'T', in_node_2), (diamond, 'F', glue), (out_node_2, '', in_node_1)})
+                 {(out_node_1, '', diamond), (diamond, 'T', in_node_2), (diamond, 'F', glue), (out_node_2, '', in_node_1)})
         in_node = in_node_1
         out_node = glue
 
@@ -93,7 +93,7 @@ class LET(compiler.LET):
         for decl in decls:
             in_i = ('START', decl)
             out_i = ('END', decl)
-            psi_1[decl] = (in_i, out_i)
+            psi_1[decl.fname] = (in_i, out_i)
         
         vertices_1, edges_1, in_node_1, out_node_1 = e.cfg(psi_1)
         
@@ -140,7 +140,7 @@ class DECL(compiler.DECL):
         in_node_f, out_node_f = psi[f]
 
         vertices = vertices_1 | {in_node_f, out_node_f}
-        edges = edges_1 | {(in_node_f, '', in_node_1), (out_node_f, '', out_node_1)}
+        edges = edges_1 | {(in_node_f, '', in_node_1), (out_node_1, '', out_node_f)}
         in_node = in_node_f
         out_node = out_node_f
 
@@ -200,8 +200,8 @@ class CALL(compiler.CALL):
             
             prev_out = out_node_i
 
-
-        edges.add((prev_out, '', call))
+        if prev_out is not None:
+            edges.add((prev_out, '', call))
         
         edges = edges | {(call, '', in_node_f), (out_node_f, '', ret), (call, '', ret)}
             
@@ -234,7 +234,7 @@ class VAR(compiler.VAR):
         """
         w = self
         vertices = {w}
-        edges = {}
+        edges = set()
         in_node = w
         out_node = w
 
@@ -300,8 +300,8 @@ class IF(compiler.IF):
         vertices_2, edges_2, in_node_2, out_node_2 = e2.cfg(psi)
         vertices_3, edges_3, in_node_3, out_node_3 = e3.cfg(psi)
 
-        diamond = 'diamond'
-        glue = 'glue'
+        diamond = ('diamond', w)
+        glue = ('glue', w)
 
         vertices = vertices_1 | vertices_2 | vertices_3 | {diamond, glue}
         edges = edges_1 | edges_2 | edges_3 | {
@@ -324,9 +324,9 @@ class SEQ(compiler.SEQ):
 
         Gemäß der Regel: Wenn e1 ; e2 dann
         V = V1 + V2
-        E = E1 + E2 + {(out, ε, in2)}
+        E = E1 + E2 + {(out1, ε, in2)}
         in = in1
-        out = w
+        out = out2
 
         where V1, E1, in1, out1 = cfg(e1, psi) und V2, E2, in2, out2 = cfg(e2, psi)
 
@@ -404,8 +404,8 @@ class DO(compiler.DO):
         vertices_1, edges_1, in_node_1, out_node_1 = e1.cfg(psi)
         vertices_2, edges_2, in_node_2, out_node_2 = e2.cfg(psi)
 
-        diamond = 'diamond'
-        glue = 'glue'
+        diamond = ('diamond', w)
+        glue = ('glue', w)
 
         vertices = vertices_1 | vertices_2 | {diamond, glue}
         edges = (edges_1 | edges_2 |
@@ -415,3 +415,292 @@ class DO(compiler.DO):
         out_node = glue
 
         return vertices, edges, in_node, out_node
+
+
+def remove_glue_nodes(vertices, edges, in_node, out_node):
+    """
+    Entfernt überflüssige glue-Knoten aus dem CFG.
+    
+    Ein glue-Knoten ist ein Durchgangsknoten, der die beiden Zweige eines
+    IF oder WHILE wieder zusammenführt. Diese Knoten haben keine semantische
+    Bedeutung und können entfernt werden, indem die Kanten direkt verbunden werden.
+    
+    Args:
+        vertices: Menge der Knoten
+        edges: Menge der Kanten (Tupel: (from, label, to))
+        in_node: Eingangsknoten
+        out_node: Ausgangsknoten
+    
+    Returns:
+        Tupel (vertices, edges, in_node, out_node) ohne glue-Knoten
+    """
+    # Finde alle glue-Knoten (aber behalte out_node wenn es ein glue ist)
+    glue_nodes = set()
+    for v in vertices:
+        if isinstance(v, tuple) and len(v) == 2 and v[0] == 'glue':
+            # Nur entfernen wenn es nicht der out_node ist
+            if v != out_node:
+                glue_nodes.add(v)
+    
+    # Entferne jeden glue-Knoten
+    new_vertices = vertices - glue_nodes
+    new_edges = set()
+    
+    for glue in glue_nodes:
+        # Finde alle eingehenden Kanten zum glue-Knoten
+        incoming = [(src, label) for src, label, dst in edges if dst == glue]
+        
+        # Finde alle ausgehenden Kanten vom glue-Knoten
+        outgoing = [(label, dst) for src, label, dst in edges if src == glue]
+        
+        # Verbinde alle eingehenden mit allen ausgehenden Kanten
+        for src, in_label in incoming:
+            for out_label, dst in outgoing:
+                # Kombiniere die Labels (normalerweise beide leer)
+                combined_label = in_label if in_label else out_label
+                new_edges.add((src, combined_label, dst))
+    
+    # Füge alle Kanten hinzu, die nicht glue-Knoten betreffen
+    for src, label, dst in edges:
+        if src not in glue_nodes and dst not in glue_nodes:
+            new_edges.add((src, label, dst))
+    
+    # Aktualisiere in_node und out_node, falls sie glue-Knoten waren
+    new_in_node = in_node
+    new_out_node = out_node
+    
+    if in_node in glue_nodes:
+        # Finde den neuen in_node (sollte normalerweise nicht vorkommen)
+        for src, label, dst in edges:
+            if dst == in_node:
+                new_in_node = src
+                break
+    
+    if out_node in glue_nodes:
+        # Finde den neuen out_node
+        for src, label, dst in edges:
+            if src == out_node:
+                new_out_node = dst
+                break
+    
+    return new_vertices, new_edges, new_in_node, new_out_node
+
+
+def export_cfg_to_dot(vertices, edges, in_node, out_node, filename):
+    """
+    Exportiert einen Control Flow Graph im DOT-Format.
+    
+    Args:
+        vertices: Menge der Knoten
+        edges: Menge der Kanten (Tupel: (from, label, to))
+        in_node: Eingangsknoten
+        out_node: Ausgangsknoten
+        filename: Pfad zur Ausgabedatei
+    """
+    # Entferne glue-Knoten vor dem Export
+    vertices, edges, in_node, out_node = remove_glue_nodes(vertices, edges, in_node, out_node)
+    
+    lines = ['digraph CFG {']
+    lines.append('  rankdir=TB;')
+    lines.append('  node [fontname="Arial"];')
+    lines.append('')
+    
+    # Mapping von Knoten zu eindeutigen IDs
+    node_to_id = {}
+    node_counter = 0
+    
+    def get_node_id(node):
+        nonlocal node_counter
+        if node not in node_to_id:
+            node_to_id[node] = f'n{node_counter}'
+            node_counter += 1
+        return node_to_id[node]
+    
+    def format_label(node):
+        """Formatiert das Label eines Knotens für DOT."""
+        if isinstance(node, str):
+            return node
+        elif isinstance(node, tuple):
+            # Tupel wie (START, decl) oder (CALL, w) oder (RET, w) oder (diamond, w) oder (glue, w)
+            if len(node) == 2:
+                label_type, obj = node
+                
+                # Für diamond und glue: spezielle Labels
+                if label_type == 'diamond':
+                    return '<?>'
+                elif label_type == 'glue':
+                    return label_type
+                
+                # Funktionsname extrahieren
+                func_name = None
+                if hasattr(obj, 'fname'):
+                    # DECL oder CALL mit fname
+                    func_name = obj.fname
+                elif hasattr(obj, '__class__'):
+                    # Anderes Objekt - versuche fname zu finden
+                    if hasattr(obj, 'fname'):
+                        func_name = obj.fname
+                
+                if func_name:
+                    # Bei CALL und RET: füge arguments hinzu
+                    if label_type in ('CALL', 'RET') and hasattr(obj, 'arguments'):
+                        args_str = ', '.join(format_label(arg) for arg in obj.arguments)
+                        return f"{label_type} {func_name}({args_str})"
+                    # Bei START und END: füge params hinzu
+                    elif label_type in ('START', 'END') and hasattr(obj, 'params'):
+                        params_str = ', '.join(param.name if hasattr(param, 'name') else str(param) for param in obj.params)
+                        return f"{label_type} {func_name}({params_str})"
+                    return f"{label_type} {func_name}"
+                else:
+                    return f"{label_type} {obj.__class__.__name__}"
+            
+            # Fallback für andere Tupel-Formate
+            parts = []
+            for part in node:
+                if isinstance(part, str):
+                    parts.append(part)
+                elif hasattr(part, 'fname'):
+                    parts.append(part.fname)
+                elif hasattr(part, '__class__'):
+                    parts.append(part.__class__.__name__)
+                else:
+                    parts.append(str(part))
+            return ' '.join(parts)
+        elif hasattr(node, '__class__'):
+            # AST-Knoten: zeige konkreten Inhalt
+            class_name = node.__class__.__name__
+            
+            # CONST: zeige den Wert
+            if class_name == 'CONST' and hasattr(node, 'value'):
+                return str(node.value)
+            
+            # VAR: zeige den Variablennamen
+            elif class_name == 'VAR' and hasattr(node, 'name'):
+                return node.name
+            
+            # BINOP: zeige die komplette Operation
+            elif class_name == 'BINOP' and hasattr(node, 'operator'):
+                left_str = format_label(node.left) if hasattr(node, 'left') else '?'
+                right_str = format_label(node.right) if hasattr(node, 'right') else '?'
+                return f"{left_str}{node.operator}{right_str}"
+            
+            # ASSIGN: zeige Zuweisung
+            elif class_name == 'ASSIGN' and hasattr(node, 'variable'):
+                var_name = node.variable.name if hasattr(node.variable, 'name') else str(node.variable)
+                return f"{var_name}="
+            
+            # IF: zeige "if"
+            elif class_name == 'IF':
+                return 'if'
+            
+            # WHILE: zeige "while"
+            elif class_name == 'WHILE':
+                return 'while'
+            
+            # DO: zeige "do"
+            elif class_name == 'DO':
+                return 'do'
+            
+            # SEQ: zeige ";"
+            elif class_name == 'SEQ':
+                return ';'
+            
+            # CALL: zeige Funktionsaufruf
+            elif class_name == 'CALL' and hasattr(node, 'fname'):
+                return f"CALL {node.fname}"
+            
+            # Default: Klassenname
+            else:
+                return class_name
+        else:
+            return str(node)
+    
+    def get_node_shape(node):
+        """Bestimmt die Form eines Knotens basierend auf seinem Typ."""
+        label = format_label(node)
+        
+        # IN/OUT als Kreis (doublecircle)
+        if label == 'IN' or label == 'OUT':
+            return 'doublecircle', label
+        
+        # diamond als Diamant (kann Tupel oder String sein)
+        if label == '<?>' or (isinstance(node, tuple) and len(node) == 2 and node[0] == 'diamond'):
+            return 'diamond', label
+        
+        # START/END/CALL/RET als Box
+        if isinstance(node, tuple) and len(node) >= 1:
+            if isinstance(node[0], str) and node[0] in ('START', 'END', 'CALL', 'RET'):
+                return 'box', label
+        
+        # glue als Box (kann Tupel oder String sein)
+        if label == 'glue' or (isinstance(node, tuple) and len(node) == 2 and node[0] == 'glue'):
+            return 'box', label
+        
+        # Normale Knoten als Ellipse
+        return 'ellipse', label
+    
+    # Spezielle Knoten für IN und OUT
+    in_id = get_node_id('IN')
+    out_id = get_node_id('OUT')
+    
+    lines.append(f'  {in_id} [shape=doublecircle, label="IN"];')
+    lines.append(f'  {out_id} [shape=doublecircle, label="OUT"];')
+    lines.append('')
+    
+    # Sammle alle Knoten aus vertices und edges
+    all_nodes = set(vertices)
+    all_nodes.add(in_node)
+    all_nodes.add(out_node)
+    
+    # Füge auch alle Knoten aus den Kanten hinzu
+    for edge in edges:
+        if len(edge) == 3:
+            from_node, _, to_node = edge
+        else:
+            from_node, to_node = edge
+        all_nodes.add(from_node)
+        all_nodes.add(to_node)
+    
+    # Alle anderen Knoten definieren
+    for vertex in all_nodes:
+        if vertex == 'IN' or vertex == 'OUT':
+            continue  # Bereits definiert
+        
+        vid = get_node_id(vertex)
+        shape, label = get_node_shape(vertex)
+        lines.append(f'  {vid} [shape={shape}, label="{label}"];')
+    
+    lines.append('')
+    
+    # Kante von IN zum Eingangsknoten
+    in_node_id = get_node_id(in_node)
+    lines.append(f'  {in_id} -> {in_node_id};')
+    
+    # Alle Kanten aus dem CFG
+    for edge in edges:
+        if len(edge) == 3:
+            from_node, label, to_node = edge
+        else:
+            # Falls Kanten ohne Label
+            from_node, to_node = edge
+            label = ''
+        
+        from_id = get_node_id(from_node)
+        to_id = get_node_id(to_node)
+        
+        if label:
+            lines.append(f'  {from_id} -> {to_id} [label="{label}"];')
+        else:
+            lines.append(f'  {from_id} -> {to_id};')
+    
+    # Kante vom Ausgangsknoten zu OUT
+    out_node_id = get_node_id(out_node)
+    lines.append(f'  {out_node_id} -> {out_id};')
+    
+    lines.append('}')
+    
+    # In Datei schreiben
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write('\n'.join(lines))
+    
+    print(f'CFG exported to: {filename}')
--- Universität/Übersetzung und Analyse von Programmen/Projekte/Projekt_4/uap25_pro3_python/main.py
+++ Universität/Übersetzung und Analyse von Programmen/Projekte/Projekt_4/uap25_pro3_python/main.py
@@ -1,116 +1,222 @@
-#Matthias Janßen
-#1871808
-
-# This is a sample Python script for testing your TRIPLA parser.
-
-# In PyCharm press Umschalt+F10 to execute it.
+# Matthias Janßen
+# 1871808
 
 import os
+import sys
 import argparse
 
 from triplalex import lexer
 import triplayacc
 import compiler
 
-import graphbuilder as gb
-
-def run(input_path: str, dot_out: str | None = None):
-
-    proj_dir = os.path.dirname(__file__)
-
-    dot_dir = os.path.join(proj_dir, 'dotFiles')
-    os.makedirs(dot_dir, exist_ok=True)
-
-    if dot_out:
-        if not os.path.isabs(dot_out):
-            out_path = os.path.join(dot_dir, dot_out)
-        else:
-            out_path = dot_out
-    else:
-        base = os.path.splitext(os.path.basename(input_path))[0]
-        out_path = os.path.join(dot_dir, base + '.ast.dot')
-
-    with open(input_path, 'r', encoding='utf-8') as f:
-        src = f.read()
-
-    lexer.input(src)
-
-    try:
-        ast_root = triplayacc.parser.parse(src, lexer=lexer)
-    except Exception as e:
-        print('Parse failed:', e)
-        return
-
-    if ast_root is None:
-        print('Parsing produced no AST (syntax error or empty input).')
-        return
-
-
-    # Export using the syntax node's built-in DOT export method
-    try:
-        ast_root.export_dot(out_path)
-        print(f'AST exported to: {out_path}')
-    except Exception as e:
-        print(f'Failed to export DOT: {e}')
+def generate_ast(ast_root, input_path, dot_dir):
+    """Generate AST DOT file."""
+    base = os.path.splitext(os.path.basename(input_path))[0]
+    ast_path = os.path.join(dot_dir, f'{base}.ast.dot')
     
-    # Generate and export TRAM code
     try:
-        base = os.path.splitext(os.path.basename(input_path))[0]
-        tram_dir = os.path.join(proj_dir, 'tram_out')
-        os.makedirs(tram_dir, exist_ok=True)
-        tram_path = os.path.join(tram_dir, base + '.tram')
-        
-        tram_code = ast_root.code({}, 0)
-        assembly_code = compiler.assemble(tram_code, tram_path)
-        print(f'TRAM code exported to: {tram_path}')
+        ast_root.export_dot(ast_path)
+        print(f'✓ AST exported to: {ast_path}')
+        return True
     except Exception as e:
-        print(f'Failed to generate TRAM code: {e}')
-        import traceback
-        traceback.print_exc()
+        print(f'✗ Failed to export AST: {e}')
+        return False
 
-def main(src):
-    ap = argparse.ArgumentParser()
-    ap.add_argument('input', nargs='?', default=src)
-    ap.add_argument('--out', '-o')
-    args = ap.parse_args()
+def generate_tram(ast_root, input_path, tram_dir):
+    """Generate TRAM code."""
+    base = os.path.splitext(os.path.basename(input_path))[0]
+    tram_path = os.path.join(tram_dir, f'{base}.tram')
+    
+    try:
+        tram_code = ast_root.code({}, 0)
+        compiler.assemble(tram_code, tram_path)
+        print(f'✓ TRAM code exported to: {tram_path}')
+        return True
+    except Exception as e:
+        print(f'✗ Failed to generate TRAM: {e}')
+        return False
 
-    run(src, args.out)
+def generate_cfg(ast_root, input_path, cfg_dir):
+    """Generate Control Flow Graph."""
+    # Import graphbuilder for CFG support
+    import graphbuilder
+    
+    base = os.path.splitext(os.path.basename(input_path))[0]
+    cfg_path = os.path.join(cfg_dir, f'{base}.cfg.dot')
+    
+    try:
+        vertices, edges, in_node, out_node = ast_root.cfg({})
+        graphbuilder.export_cfg_to_dot(vertices, edges, in_node, out_node, cfg_path)
+        print(f'✓ CFG exported to: {cfg_path}')
+        return True
+    except Exception as e:
+        print(f'✗ Failed to generate CFG: {e}')
+        return False
 
-def test_parser(name):
-    source = "\n".join(open(name).readlines())
-    ast = triplayacc.parser.parse(source)#, debug=True)
-    print("AST:")
-    print(ast)
-    if ast is not None:
-        # Also export the AST to DOT format
-        proj_dir = os.path.dirname(__file__)
-        dot_dir = os.path.join(proj_dir, 'dotFiles')
-        os.makedirs(dot_dir, exist_ok=True)
-        base = os.path.splitext(os.path.basename(name))[0]
-        out_path = os.path.join(dot_dir, base + '.ast.dot')
-        try:
-            ast.export_dot(out_path)
-            print(f'AST exported to: {out_path}')
-        except Exception as e:
-            print(f'Failed to export DOT: {e}')
+def interactive_mode():
+    """Interactive mode: ask user what to generate."""
+    print("TRIPLA Compiler und CFG Generator")
+    print("=" * 50)
+    print()
+    
+    # Get input file
+    print("Verfügbare TRIPLA Programme:")
+    tripla_dir = 'triplaprograms'
+    if os.path.exists(tripla_dir):
+        files = [f for f in os.listdir(tripla_dir) if f.endswith('.tripla')]
+        for i, f in enumerate(files, 1):
+            print(f"  {i}. {f}")
+        print()
         
-        # Generate and export TRAM code
+        choice = input("Dateinummer eingeben (oder vollständiger Pfad): ").strip()
+        if choice.isdigit() and 1 <= int(choice) <= len(files):
+            input_path = os.path.join(tripla_dir, files[int(choice) - 1])
+        else:
+            input_path = choice
+    else:
+        input_path = input("Pfad zur TRIPLA Datei: ").strip()
+    
+    if not os.path.exists(input_path):
+        print(f"✗ Datei nicht gefunden: {input_path}")
+        return
+    
+    print(f"\nVerarbeite: {input_path}")
+    print()
+    
+    # Ask what to generate
+    print("Was soll generiert werden?")
+    print("  1. AST (Abstract Syntax Tree)")
+    print("  2. TRAM Code")
+    print("  3. CFG (Control Flow Graph)")
+    print("  4. Alles (AST + TRAM + CFG)")
+    print()
+    
+    choice = input("Auswahl [1-4]: ").strip()
+    
+    generate_ast_flag = choice in ('1', '4')
+    generate_tram_flag = choice in ('2', '4')
+    generate_cfg_flag = choice in ('3', '4')
+    
+    if not any([generate_ast_flag, generate_tram_flag, generate_cfg_flag]):
+        print("✗ Ungültige Auswahl")
+        return
+    
+    print()
+    print("Verarbeitung läuft...")
+    print()
+    
+    # Parse source
+    process_file(input_path, generate_ast_flag, generate_tram_flag, generate_cfg_flag)
+
+def process_file(input_path, gen_ast=True, gen_tram=True, gen_cfg=True):
+    """Process TRIPLA file and generate requested outputs."""
+    
+    proj_dir = os.path.dirname(__file__) or '.'
+    dot_dir = os.path.join(proj_dir, 'dotFiles')
+    tram_dir = os.path.join(proj_dir, 'tram_out')
+    cfg_dir = os.path.join(proj_dir, 'cfg_examples')
+    
+    os.makedirs(dot_dir, exist_ok=True)
+    os.makedirs(tram_dir, exist_ok=True)
+    os.makedirs(cfg_dir, exist_ok=True)
+    
+    # Read source file
+    with open(input_path, 'r', encoding='utf-8') as f:
+        source = f.read()
+    
+    # Parse for AST and TRAM (uses compiler module)
+    if gen_ast or gen_tram:
+        lexer.input(source)
         try:
-            tram_dir = os.path.join(proj_dir, 'tram_out')
-            os.makedirs(tram_dir, exist_ok=True)
-            tram_path = os.path.join(tram_dir, base + '.tram')
-            
-            tram_code = ast.code({}, 0)
-            assembly_code = compiler.assemble(tram_code, tram_path)
-            print(f'TRAM code exported to: {tram_path}')
+            ast_root = triplayacc.parser.parse(source, lexer=lexer)
         except Exception as e:
-            print(f'Failed to generate TRAM code: {e}')
-            import traceback
-            traceback.print_exc()
+            print(f'✗ Parse failed: {e}')
+            return
+        
+        if ast_root is None:
+            print('✗ Parsing produced no AST.')
+            return
+        
+        if gen_ast:
+            generate_ast(ast_root, input_path, dot_dir)
+        
+        if gen_tram:
+            generate_tram(ast_root, input_path, tram_dir)
+    
+    # Parse for CFG (uses graphbuilder module)
+    if gen_cfg:
+        # Switch compiler module to graphbuilder for CFG support
+        original_compiler = sys.modules.get('compiler')
+        sys.modules['compiler'] = __import__('graphbuilder')
+        
+        # Re-import to use graphbuilder classes
+        import importlib
+        importlib.reload(triplayacc)
+        
+        lexer.input(source)
+        try:
+            ast_root_cfg = triplayacc.parser.parse(source, lexer=lexer)
+        except Exception as e:
+            print(f'✗ CFG parse failed: {e}')
+            # Restore original compiler module
+            if original_compiler:
+                sys.modules['compiler'] = original_compiler
+            return
+        
+        if ast_root_cfg is None:
+            print('✗ CFG parsing produced no AST.')
+            # Restore original compiler module
+            if original_compiler:
+                sys.modules['compiler'] = original_compiler
+            return
+        
+        generate_cfg(ast_root_cfg, input_path, cfg_dir)
+        
+        # Restore original compiler module
+        if original_compiler:
+            sys.modules['compiler'] = original_compiler
+            importlib.reload(triplayacc)
+    
+    print()
+    print("✓ Verarbeitung abgeschlossen!")
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='TRIPLA Compiler und CFG Generator',
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        epilog="""
+Beispiele:
+  python main.py                                    # Interaktiver Modus
+  python main.py --file prog.tripla --all          # Alles generieren
+  python main.py --file prog.tripla --ast --tram   # Nur AST und TRAM
+  python main.py --file prog.tripla --cfg          # Nur CFG
+        """
+    )
+    parser.add_argument('--file', '-f', help='Input TRIPLA source file')
+    parser.add_argument('--ast', action='store_true', help='Generate AST')
+    parser.add_argument('--tram', action='store_true', help='Generate TRAM code')
+    parser.add_argument('--cfg', action='store_true', help='Generate CFG')
+    parser.add_argument('--all', '-a', action='store_true', help='Generate all outputs')
+    
+    args = parser.parse_args()
+    
+    # Interactive mode if no arguments
+    if not args.file:
+        interactive_mode()
+    else:
+        # Command line mode
+        if args.all:
+            gen_ast = gen_tram = gen_cfg = True
+        else:
+            gen_ast = args.ast
+            gen_tram = args.tram
+            gen_cfg = args.cfg
+            
+            # If no flags specified, generate all
+            if not any([gen_ast, gen_tram, gen_cfg]):
+                gen_ast = gen_tram = gen_cfg = True
+        
+        process_file(args.file, gen_ast, gen_tram, gen_cfg)
 
 if __name__ == '__main__':
-    src='triplaprograms/argsParamsExample.tripla'
-    main(src)
-    test_parser(src)
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
